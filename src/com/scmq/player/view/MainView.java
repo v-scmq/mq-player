13461e3c2e783465bb5f762cb94e70a3d62d474a
scmq <1715499885@qq.com>
2020-09-06 20:48:07 +0800

    修改刷新图标大小

--- src/com/scmq/player/view/MainView.java
+++ src/com/scmq/player/view/MainView.java
@@ -156,6 +156,8 @@ public class MainView {
 				"M8 3a5 5 0 1 0 4.546 2.914.5.5 0 0 1 .908-.417A6 6 0 1 1 8 2v1z M8 4.466V.534a.25.25 0 0 1 .41-.192l2.36 1.966c.12.1.12.284 0 .384L8.41 4.658A.25.25 0 0 1 8 4.466z");
 		refreshPath.setStrokeWidth(0.65);
 		refreshPath.setRotate(20);
+		refreshPath.setScaleX(1.2);
+		refreshPath.setScaleY(1.2);
 		refreshButton = new Button(null, refreshPath);
 		refreshButton.getStyleClass().setAll("svg-button");
 
